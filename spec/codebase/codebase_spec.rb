1b4ae3af095235049def5994f40987af8ac333d4
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-18 12:27:42 +0700

    Add missing tests for CI

---
+++ spec/codebase/codebase_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Codebase', codebase: true do
+  it 'does not offend scss-lint' do
+    expect(`scss-lint`).to be_empty
+  end
+
+  it 'does not offend eslint' do
+    expect(`yarn run eslint ./`).to include 'Done'
+  end
+end
