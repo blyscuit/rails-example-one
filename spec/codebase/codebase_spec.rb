20eafc78150d216c8067b19a4a7649656b521dd2
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-18 12:52:34 +0700

    Add lint libs

--- spec/codebase/codebase_spec.rb
+++ spec/codebase/codebase_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 describe 'Codebase', codebase: true do
   it 'does not offend scss-lint' do
-    expect(`scss-lint`).to be_empty
+    expect(`scss-lint ./`).to be_empty
   end
 
   it 'does not offend eslint' do
-    expect(`yarn run eslint ./`).to include 'Done'
+    expect(`yarn run eslint`).to include 'Done'
   end
 end
