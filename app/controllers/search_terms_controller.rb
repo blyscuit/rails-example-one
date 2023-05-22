89a10c6ab2753c9f31ac1f2ad780b8deb95699af
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-22 15:16:07 +0700

    Fix search terms not correct shown

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -2,6 +2,11 @@ class SearchTermsController < ApplicationController
   def index
   end
   
+  # GET /a_search_terms
+  def index
+    @search_terms = SearchTerm.all
+  end
+
   def import
     return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
     return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
