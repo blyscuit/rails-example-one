557e599fb7f2d8895b6d65fe34fbf087f0c1f351
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-06-09 12:33:38 +0700

    Add google token auth

--- app/controllers/api/v1/google_users_controller.rb
+++ app/controllers/api/v1/google_users_controller.rb
@@ -5,8 +5,19 @@ module Api
         skip_before_action :doorkeeper_authorize!, only: %i[create]
     
         def create
+          uri = URI.parse("https://www.googleapis.com/oauth2/v3/token")
+          auth_params = {
+            code: params[:code],
+            client_id: ENV['GOOGLE_OAUTH_CLIENT_ID'],
+            client_secret: ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
+            redirect_uri: 'http://localhost:3000/admins/auth/google_oauth2/callback',
+            grant_type: 'authorization_code'
+          }
+          response = Net::HTTP.post_form(uri, auth_params)
+          data = JSON.parse(response.body)
+
           google = URI.parse('https://www.googleapis.com/oauth2/v3/userinfo?access_token=' +
-            params[:token])
+            data['access_token'])
           response = Net::HTTP.get_response(google)
           data = JSON.parse(response.body)
 
